5e88b3b9b4c9655cba1802e57d41233e3c17f43a
konye_k <konyekokim@gmail.com>
2018-07-31 12:28:41 +0100

    Initial commit

---
+++ app/src/main/java/com/chokus/konye/mvptutorial/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.chokus.konye.mvptutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
